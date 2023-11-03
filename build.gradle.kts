2fff2113e9b43a045abfa9a4203b8023f78e6af9
Robert Novotny <robert.novotny@jetbrains.com>
2023-11-03 18:04:29 +0100

    Add dependencies for Akka Types and Cluster Typed

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,12 @@ repositories {
 }
 
 dependencies {
+    implementation("com.typesafe.akka:akka-actor-typed_2.13:2.6.21")
+    implementation("com.typesafe.akka:akka-cluster-typed_2.13:2.6.21")
+    implementation("org.scala-lang:scala-library:2.13.12")
+    runtimeOnly("ch.qos.logback:logback-classic:1.4.11")
+
+    testImplementation(kotlin("test"))
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
