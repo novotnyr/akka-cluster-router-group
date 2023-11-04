52b70ac6ec5c242f7c5272a5e7ea3544a9c02a44
Robert Novotny <robert.novotny@jetbrains.com>
2023-11-04 11:16:51 +0100

    Register worker instances in the Receptionist

--- src/main/java/com/github/novotnyr/akka/Worker.java
+++ src/main/java/com/github/novotnyr/akka/Worker.java
@@ -5,16 +5,22 @@ import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
+import akka.actor.typed.receptionist.Receptionist;
+import akka.actor.typed.receptionist.ServiceKey;
 
 import static com.github.novotnyr.akka.Worker.WorkData;
 
 public class Worker extends AbstractBehavior<WorkData> {
+
+    public static final ServiceKey<WorkData> WORK_DATA_SERVICE_KEY = ServiceKey.create(Worker.WorkData.class, "worker");
+
     public static Behavior<WorkData> create() {
         return Behaviors.setup(Worker::new);
     }
 
     private Worker(ActorContext<WorkData> context) {
         super(context);
+        context.getSystem().receptionist().tell(Receptionist.register(WORK_DATA_SERVICE_KEY, getContext().getSelf()));
     }
 
     @Override
